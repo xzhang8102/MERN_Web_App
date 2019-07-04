b55fc8ed6eac60a2960d50c37e1ecb3dbb013290
zxw <xzhang305@gmail.com>
2019-07-04 16:44:53 -0400

    delete logged in user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -140,4 +140,24 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route       DELETE api/profile
+// @description deleter user profile
+// @access      private
+
+router.delete('/', auth, async (req, res) => {
+  try {
+    //Delete posts
+
+    //Delete profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    //Delete user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
