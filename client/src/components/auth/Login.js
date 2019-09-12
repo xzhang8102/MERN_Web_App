98a24d218100eaf47f1ea7dbcecaabe85df8184c
zxw <xzhang305@gmail.com>
2019-09-12 11:35:05 -0700

    tweaked register page

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Login = () => {
   return (
     <section className="container">
-      <h1>Login</h1>;
+      <h1>Login</h1>
     </section>
   );
 };
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -23,61 +24,63 @@ const Register = () => {
   };
 
   return (
-    <section className="container">
-      <h1 className="large text-primary">Sign Up</h1>
-      <p className="lead">
-        <i className="far fa-user"></i> Create Your Account
-      </p>
-      <form className="form" onSubmit={e => onFormSubmit(e)}>
-        <div className="form-group">
-          <input
-            type="text"
-            placeholder="Name"
-            name="name"
-            value={name}
-            onChange={e => onTypeChange(e)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="email"
-            placeholder="Email Address"
-            name="email"
-            value={email}
-            onChange={e => onTypeChange(e)}
-            required
-          />
-          <small className="form-text">
-            This site uses Gravatar so if you want a profile image, use a
-            Gravatar email
-          </small>
-        </div>
-        <div className="form-group">
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            value={password}
-            minLength="6"
-            onChange={e => onTypeChange(e)}
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            name="password2"
-            value={password2}
-            minLength="6"
-            onChange={e => onTypeChange(e)}
-          />
-        </div>
-        <input type="submit" className="btn btn-primary" value="Register" />
-      </form>
-      <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
-      </p>
+    <section className="container-with-image">
+      <div className="container">
+        <h1 className="large text-primary">Sign Up</h1>
+        <p className="lead">
+          <i className="far fa-user"></i> Create Your Account
+        </p>
+        <form className="form" onSubmit={e => onFormSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={name}
+              onChange={e => onTypeChange(e)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              value={email}
+              onChange={e => onTypeChange(e)}
+              required
+            />
+            <small className="form-text">
+              This site uses Gravatar so if you want a profile image, use a
+              Gravatar email
+            </small>
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              minLength="6"
+              onChange={e => onTypeChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              name="password2"
+              value={password2}
+              minLength="6"
+              onChange={e => onTypeChange(e)}
+            />
+          </div>
+          <input type="submit" className="btn btn-primary" value="Register" />
+        </form>
+        <p className="my-1">
+          Already have an account? <Link to="/login">Sign In</Link>
+        </p>
+      </div>
     </section>
   );
 };
